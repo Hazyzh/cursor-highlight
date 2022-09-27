86aa9d15ef359a9cf38985c01c5675a812d284cc
harry.hou <harry.hou@ringcentral.com>
2022-09-27 22:41:43 +0800

    remove comment

--- src/layers/Shapes/AuxiliaryBase/ArrowAuxiliaryShape.ts
+++ src/layers/Shapes/AuxiliaryBase/ArrowAuxiliaryShape.ts
@@ -12,7 +12,6 @@ export abstract class ArrowAuxiliaryShape extends PointAuxiliaryBase {
     if (!endPoint) return;
 
     const pointsList = [
-      // horizontal start line
       startPoint,
       endPoint,
     ];
